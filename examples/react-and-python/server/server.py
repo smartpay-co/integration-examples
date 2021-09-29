8236a27730c0514584f93bf4151d689a336e9f51
othree <othree@smartpay.co>
2021-09-29 17:06:11 +0800

    Re-build

--- examples/react-and-python/server/server.py
+++ examples/react-and-python/server/server.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from smartpay import Smartpay
 
 
-smartpay = Smartpay('<YOUR_PRIVATE_API_KEY>',
-                    public_key='<YOUR_PUBLIC_API_KEY>')
+# Replace the keys with yours
+PRIVATE_API_KEY = os.environ.get('PRIVATE_API_KEY', '<YOUR_PRIVATE_API_KEY>')
+PUBLIC_API_KEY = os.environ.get('PUBLIC_API_KEY', '<YOUR_PUBLIC_API_KEY>')
+
+smartpay = Smartpay(PRIVATE_API_KEY,
+                    public_key=PUBLIC_API_KEY)
 
 
 app = Flask(__name__, static_url_path='')
