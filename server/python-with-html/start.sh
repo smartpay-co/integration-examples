888b2f75b9aeba770558aa923a75093d55dbe946
othree <othree@gmail.com>
2021-10-25 13:04:11 +0800

    fix(sc-2266): Use python -m pip (#15)
    * [sc-2266] Use python3 -m pip
    
    * [sc-2266] rebuild

--- server/python-with-html/start.sh
+++ server/python-with-html/start.sh
@@ -1,1 +1,1 @@
-pip3 install -q --user -r requirements.txt && FLASK_APP=server python3 -m flask run --host=127.0.0.1 --port=5000
+python3 -m pip install -q --user -r requirements.txt && FLASK_APP=server python3 -m flask run --host=127.0.0.1 --port=5000
